e8781b209b749757eed06ab671d2aee45555c01b
Deeksha Gupta <deeksha.gup93@gmail.com>
2018-12-26 16:35:58 +0530

    Removing parameter

--- src/main/java/com/resolariot/demo/Initializer.java
+++ src/main/java/com/resolariot/demo/Initializer.java
@@ -1,6 +1,5 @@
 package com.resolariot.demo;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,7 @@ import com.resolariot.postgres.App;
 public class Initializer {
 
 	@RequestMapping(path = "/init", method = RequestMethod.GET)
-	public void init(@RequestBody String name) {
+	public void init() {
 		new App().connect();
 	}
 
